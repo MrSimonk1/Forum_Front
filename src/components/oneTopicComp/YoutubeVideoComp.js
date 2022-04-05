c716bdfc8618a2460bc8809bd1021c6e5abacb75
Simonas Krasauskas <simonaskrasauskas@gmail.com>
2022-04-05 10:48:42 +0300

    styled youtube frame

--- src/components/oneTopicComp/YoutubeVideoComp.js
+++ src/components/oneTopicComp/YoutubeVideoComp.js
@@ -1,13 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styles from "./OneTopicStyle.module.css";
 
 const YoutubeVideoComp = ({id}) => {
 
     return (
         <div>
             <iframe
-            width="200"
-            height="100"
+            className={styles.frame}
             src={`https://www.youtube.com/embed/${id}`}
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
